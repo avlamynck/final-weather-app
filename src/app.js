64026a575757df8e1bcd11e8a7911ff34608d941
avlamynck <78685733+avlamynck@users.noreply.github.com>
2021-03-29 07:47:12 +1100

    created changing forecast icon

--- src/app.js
+++ src/app.js
@@ -9,8 +9,25 @@ function formatDate (timestamp) {
     return ` ${day} ${hours}:${minutes}`; 
 }
 
+function formattedDate (timestamp) {
+    let date = new Date (timestamp);
+    let sunriseDate = new Date (timestamp);
+    let hours = sunriseDate.getHours();
+     if (hours <10) {hours=`0${hours}`};
+    let minutes = date.getMinutes();
+    if (minutes <10) {minutes = `0${minutes}`};
+    return `${hours}:${minutes}`;
+}
+
+function formattedDateSunset (timestamp) {
+    let date = new Date (timestamp);
+    let sunsetDate = new Date (timestamp);
+    let hour = sunsetDate.getHours();
+    let minutes = date.getMinutes();
+    return `${hour}:${minutes}`;
+}
+
 function displayTemperature(response) {
-    console.log(response.data)
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector(".weather-description");
@@ -19,21 +36,23 @@ function displayTemperature(response) {
     let feelslikeElement = document.querySelector("#value-feeling");
     let countryElement=document.querySelector("#country");
     let dateElement = document.querySelector("#date-time");
+    let iconElement = document.querySelector("#forecast-icon");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML= response.data.weather[0].description;
-    sunriseElement.innerHTML=response.data.sys.sunrise;
-    sunsetElement.innerHTML=response.data.sys.sunset;
+    sunriseElement.innerHTML= formattedDate (response.data.sys.sunrise *1000);
+    sunsetElement.innerHTML= formattedDateSunset(response.data.sys.sunset *1000);
     feelslikeElement.innerHTML= Math.round(response.data.main.feels_like);
     countryElement.innerHTML=response.data.sys.country;
     dateElement.innerHTML= formatDate(response.data.dt * 1000);
-
-    console.log(response);
-
+    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 
 }
 
 let apiKey = "c464dd164b44484161303b9f1d1f0121";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let city = "Sydney"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
